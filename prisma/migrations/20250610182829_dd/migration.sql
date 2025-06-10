4d64b93b1a536424daf185d5a85c84db5c3d1f3b
AlexArancibia <alexanderarancibia37@gmail.com>
2025-06-10 13:44:49 -0500

    hash updated

---
+++ prisma/migrations/20250610182829_dd/migration.sql
@@ -0,0 +1,29 @@
+-- DropIndex
+DROP INDEX "transactions_transactionHash_key";
+
+-- AlterTable
+ALTER TABLE "audit_logs" ALTER COLUMN "id" SET DEFAULT 'audit_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "bank_accounts" ALTER COLUMN "id" SET DEFAULT 'bank_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "companies" ALTER COLUMN "id" SET DEFAULT 'comp_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "conciliation_items" ALTER COLUMN "id" SET DEFAULT 'conc_item_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "conciliations" ALTER COLUMN "id" SET DEFAULT 'conc_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "expenses" ALTER COLUMN "id" SET DEFAULT 'exp_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "suppliers" ALTER COLUMN "id" SET DEFAULT 'supp_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "transactions" ALTER COLUMN "id" SET DEFAULT 'txn_' || substr(gen_random_uuid()::text, 1, 13);
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT 'user_' || substr(gen_random_uuid()::text, 1, 13);
