c16da14281651f5629755b9f892e6f3e891cae10
AlexArancibia <alexanderarancibia37@gmail.com>
2025-06-19 03:37:31 -0500

    controllers sunat and accounting added

--- src/accounting/accounting.module.ts
+++ src/accounting/accounting.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common"
 import { AccountingService } from "./accounting.service"
 import { PrismaModule } from "../prisma/prisma.module"
+import { AccountingController } from "./accounting.controller"
 
 @Module({
   imports: [PrismaModule],
+  controllers:[AccountingController],
   providers: [AccountingService],
   exports: [AccountingService],
 })
